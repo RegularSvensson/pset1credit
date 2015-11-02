6c4dbb2f5d808546d535619d0b4c420c60933763
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-02 14:38:50 +0100

    remove 2 last digits of card number for next loop

--- credit.c
+++ credit.c
@@ -27,6 +27,8 @@ int main(void)
         int n = (card / 10) % 10;
         // add this number * 2 to value
         value += n * 2;
+        // remove 2 last digits of card number for next loop
+        card /= 100;
     }
     printf("The value is: %i\n", value);
 }
