68931686ed0a7dfe7251b516bd7f049e705b1824
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-02 14:31:25 +0100

    Create a Green test for 4-digit numbers

--- credit.c
+++ credit.c
@@ -13,4 +13,29 @@ int main(void)
     
     // print number to user
     printf("You chose: %lli\n", card);
+
+    // initialize value to 0
+    int value = 0;
+    
+    // get second to last number
+    int n = (card / 10) % 10;
+    
+    // update value to n * 2
+    value += n * 2;
+    
+    // update card number
+    card /= 100;
+    
+    // get second to last number, 2
+    n = (card / 10) % 10;
+    
+    // update value to n * 2, 2
+    value += n * 2;
+    
+    // update card number, 2
+    card /= 100;
+    
+    printf("Second to last number is: %i\n", n);
+    printf("The value is: %i\n", value);
+    printf("The card is now: %lli\n", card);
 }
